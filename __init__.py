1405c9eb84f347ace2ac5e6de45e793ab669eb44
Jeffrey Hazboun <jeffreyhazboun@Jeffreys-iMac.local>
2017-04-19 13:07:31 -0500

    Added proper __init__.py

---
+++ __init__.py
@@ -0,0 +1,9 @@
+#__init__.py
+from .signal import Signal
+from .pulsar import Pulsar
+from .ism import ISM
+from . import PSS_utils as utils
+from .PSS_plot import *
+from .scintillation import *
+
+__all__ = ['Signal','Pulsar','ISM','utils']
