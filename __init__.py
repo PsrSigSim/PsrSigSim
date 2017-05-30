6c606eb229323b9f9b9e45a4e4fb9de512fd22ad
Jeffrey Hazboun <jeffrey@Jeffrey-Hazbouns-MacBook-Air.local>
2017-05-29 19:49:27 -0500

    added burst.py and telescope.py

--- __init__.py
+++ __init__.py
@@ -2,8 +2,10 @@
 from .signal import Signal
 from .pulsar import Pulsar
 from .ism import ISM
+from .telescope import Telescope
+from .burst import Burst
 from . import PSS_utils as utils
 from .PSS_plot import *
 from .scintillation import *
 
-__all__ = ['Signal','Pulsar','ISM','utils']
+__all__ = ['Signal','Pulsar','ISM','utils','Telescope']
