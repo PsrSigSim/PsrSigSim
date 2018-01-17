d36025cbbce1ec7b228049557d29c231628b4c37
Paul T. Baker <paultbaker@gmail.com>
2018-01-17 11:03:16 -0500

    template generated files

---
+++ AUTHORS.rst
@@ -0,0 +1,17 @@
+=======
+Credits
+=======
+
+Development Leads
+-----------------
+
+* Jeffrey S. Hazboun <jeffrey.hazboun@nanograv.org>
+* Paul T. Baker <paul.baker@nanograv.org>
+
+Contributors
+------------
+
+* Cassidy Wagner <cassidymwagner@gmail.com>
+* Amelia Henkel <amelia.henkel@uconn.edu>
+* Paul Brook <paul.brook@nanograv.org>
+* Joe Romano <joseph.romano@nanograv.org>
---
+++ setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    # TODO: put package requirements here
+]
+
+setup_requirements = [
+    'pytest-runner',
+    # TODO(Hazboun6): put setup requirements (distutils extensions, etc.) here
+]
+
+test_requirements = [
+    'pytest',
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='psrsimsig',
+    version='0.1.0',
+    description="The NANOGrav pulsar signal simulator.",
+    long_description=readme + '\n\n' + history,
+    author="Jeffrey S. Hazboun",
+    author_email='jeffrey.hazboun@nanograv.org',
+    url='https://github.com/PsrSigSim/VersionZeroPointZero',
+    packages=find_packages(include=['psrsimsig']),
+    include_package_data=True,
+    install_requires=requirements,
+    license="MIT license",
+    zip_safe=False,
+    keywords='psrsimsig',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+    setup_requires=setup_requirements,
+)
