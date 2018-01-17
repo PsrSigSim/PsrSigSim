036637ec95b719d2ed1c57b9d961808f8e02b987
Paul T. Baker <paultbaker@gmail.com>
2018-01-17 12:47:01 -0500

    empty tests

--- tests/test_psrsimsig.py
+++ tests/test_psrsimsig.py
@@ -6,7 +6,7 @@
 import pytest
 
 
-from psrsimsig import psrsimsig
+import psrsimsig
 
 
 @pytest.fixture
@@ -17,9 +17,11 @@ def response():
     """
     # import requests
     # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+    pass
 
 
 def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
+    pass
