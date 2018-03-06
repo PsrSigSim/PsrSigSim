7a11fd489b8c974509fd328411dcfe8ff382644c
Hazboun6 <jeffrey.hazboun@gmail.com>
2018-03-05 21:49:26 -0800

    lint edits

--- psrsigsim/signal.py
+++ psrsigsim/signal.py
@@ -6,62 +6,65 @@ from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 import numpy as np
 import h5py
-from astropy import units as u
+# from astropy import units as u
 from . import PSS_plot
+
 
 class MetaData(object):
     """
     The MetaData class to contain information about the signal
     """
     def __init__(self):
-        self.f0 = None # central freq (MHz)
-        self.bw = None # bandwidth (MHz)
-        self.Nf = None # number of frequency bins
-        self.Nt = None # number of time/phase bins
-        self.ObsTime = None #Total time in milliseconds
+        self.f0 = None  # central freq (MHz)
+        self.bw = None  # bandwidth (MHz)
+        self.Nf = None  # number of frequency bins
+        self.Nt = None  # number of time/phase bins
+        self.ObsTime = None  # Total time in milliseconds
 
-    def AddInfo(self,Info):
+    def AddInfo(self, Info):
         """Function to Add Information into the metadata from a dictionary
             Since each new module will have a dictionary of terms to
             add in to the metadata
         """
-        for ii,jj in Info.items():
+        for ii, jj in Info.items():
             setattr(self, ii, jj)
 
         #TODO Add error message if someone trys to overlap parameters
 
 
-
 class Signal(object):
     """The signal class
     """
-    def __init__(self, f0=1400, bw=400, Nf=20, f_samp=1, ObsTime=200, data_type='float32', SignalType = "intensity", mode='explore'):
+    def __init__(self, f0=1400, bw=400, Nf=20, f_samp=1, ObsTime=200,
+                 data_type='float32', SignalType="intensity", mode='explore'):
         """initialize Signal(), executed at assignment of new instance
         data_type = 'int8' or 'int16' supported.
-                    Automatically changed to 'uint8' or 'uint16' if intensity signal.
+                    Automatically changed to 'uint8' or 'uint16' if intensity
+                    signal.
         @param f0 -- central frequency (MHz)
         @param bw -- bandwidth (MHz)
         @param f_samp -- samplinf frequency (MHz)
         @param Nf -- number of freq. bins
         @param Nt -- number of phase bins
         Totime = total time of the observation in milliseconds
-        SignalType = 'intensity' which carries a Nf x Nt filterbank of pulses or 'voltage'
-                    which carries a 2 x Nt array of voltage vs. time pulses representing 4 stokes channels
+        SignalType = 'intensity' which carries a Nf x Nt filterbank of pulses
+                     or 'voltage' which carries a 2 x Nt array of voltage vs.
+                     time pulses representing 4 stokes channels
         """
         self.MetaData = MetaData()
-        self.f0 = f0 # (MHz)
-        self.bw = bw # (MHz)
-        self.f_samp = f_samp # (MHz)
-         # freq bins
+        self.f0 = f0  # (MHz)
+        self.bw = bw  # (MHz)
+        self.f_samp = f_samp  # (MHz)
         self.data_type = data_type
         self.SignalType = SignalType
         self.SignalDict = {}
-        self.ObsTime = ObsTime #Total time in milliseconds
+        self.ObsTime = ObsTime   # Total time in milliseconds
         Nt = int(self.ObsTime*1e-3 * self.f_samp*1e6)+1
 
-        if Nt%2 == 0: # Make signal even in length (for FFTs)
-            self.Nt = Nt # phase bins
-        else: self.Nt = Nt + 1
+        if Nt % 2 == 0:  # Make signal even in length (for FFTs)
+            self.Nt = Nt  # phase bins
+        else:
+            self.Nt = Nt + 1
 
         if SignalType == 'voltage':
             self.Nf = int(Nf)
@@ -114,28 +117,33 @@ class Signal(object):
         self.TimeBinSize = self.ObsTime/self.Nt
         self.freqBinSize = self.bw/self.Nf
         self.first_freq = self.f0 - self.freqBinSize * self.Nf/2
-        if self.first_freq == 0.0 :
+        if self.first_freq == 0.0:
             self.first_freq = self.first_freq + self.freqBinSize * 1e-10
-            print("First Frequency adjusted",self.freqBinSize * 1e-10,"MHz away from zero to avoid division errors.")
-        elif self.first_freq < 0.0 :
+            print("First Frequency adjusted", self.freqBinSize * 1e-10, "MHz \
+                  away from zero to avoid division errors.")
+        elif self.first_freq < 0.0:
             raise ValueError("First Frequency Less Than Zero")
         self.last_freq = self.f0 + self.freqBinSize * self.Nf/2
-        self.freq_Array = np.linspace(self.first_freq + self.freqBinSize/2, self.last_freq + self.freqBinSize/2, self.Nf, endpoint=False)
+        self.freq_Array = np.linspace(self.first_freq + self.freqBinSize/2,
+                                      self.last_freq + self.freqBinSize/2,
+                                      self.Nf, endpoint=False)
 
-        if self.Nt*self.Nf > 500000: #Limits the array size to 2.048 GB
+        if self.Nt*self.Nf > 500000:  # Limits the array size to 2.048 GB
             SignalPath = "signal.hdf5"
-            if SignalType=='burst': #Use a different file name for a burst
+            if SignalType=='burst':  # Use a different file name for a burst
                 SignalPath = "burst_signal.hdf5"
-            SignalFile = h5py.File(SignalPath,'a')
-            self.signal = SignalFile.create_dataset(None, (rows, self.Nt), dtype=self.data_type)
-            #self.signal = np.memmap(SignalPath, dtype = self.data_type, mode = 'w+', shape = (self.Nf, self.Nt))
+            SignalFile = h5py.File(SignalPath, 'a')
+            self.signal = SignalFile.create_dataset(None, (rows, self.Nt),
+                                                    dtype=self.data_type)
+            #self.signal = np.memmap(SignalPath, dtype = self.data_type,
+            #mode = 'w+', shape = (self.Nf, self.Nt))
         else:
-            self.signal = np.zeros((rows, self.Nt),dtype=self.data_type)
+            self.signal = np.zeros((rows, self.Nt), dtype=self.data_type)
 
         self.SignalDict['mode'] = mode
         self.MetaData.AddInfo(self.SignalDict)
 
-    ### Plotting Methods
+    # Plotting Methods
     def pulse_plot(self, **kwargs):
         return PSS_plot.pulse_plot(self, **kwargs)
 
@@ -148,7 +156,6 @@ class Signal(object):
     #Set the signal parameters as properties and assign them to the MetaData
 
     @property
-
     def f0(self):
         return self._f0
 
@@ -158,7 +165,6 @@ class Signal(object):
         self.MetaData.f0 = self._f0
 
     @property
-
     def bw(self):
         return self._bw
 
@@ -168,7 +174,6 @@ class Signal(object):
         self.MetaData.bw = self._bw
 
     @property
-
     def Nf(self):
         return self._Nf
 
@@ -178,7 +183,6 @@ class Signal(object):
         self.MetaData.Nf = self._Nf
 
     @property
-
     def Nt(self):
         return self._Nt
 
@@ -188,7 +192,6 @@ class Signal(object):
         self.MetaData.Nt = self._Nt
 
     @property
-
     def ObsTime(self):
         return self._ObsTime
 
@@ -198,7 +201,6 @@ class Signal(object):
         self.MetaData.ObsTime = self._ObsTime
 
     @property
-
     def data_type(self):
         return self._data_type
 
@@ -208,7 +210,6 @@ class Signal(object):
         self.MetaData.data_type = self._data_type
 
     @property
-
     def SignalType(self):
         return self._SignalType
 
