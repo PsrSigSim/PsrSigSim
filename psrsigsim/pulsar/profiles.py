61092c79bfce89fb4d845e0ba3d7039c78626c91
Paul T. Baker <paultbaker@gmail.com>
2018-11-06 12:06:54 -0500

    pulsar updates

--- psrsigsim/pulsar/profiles.py
+++ psrsigsim/pulsar/profiles.py
@@ -5,97 +5,71 @@ import numpy as np
 from ..utils.utils import make_quant
 
 class PulseProfile(object):
-    """base class for pulse profiles"""
+    """base class for pulse profiles
 
-    def make_pulses(self, signal, tobs, folded=False):
-        """generate pulses from profile
+    This class carries the pulse generation methods
+    """
+    _profile = None
+
+    def __call__(self, Nphase=None):
+        """a :class:`PulseProfile` returns the actual profile when called
+        """
+        if Nphase is None:
+            if self._profile is None:
+                msg = "profile not generated, returning `None`"
+                print("Warning: "+msg)
+            return self._profile
+        else:
+            self.gen_profile(Nphase)
+            return self._profile
+
+    def gen_profile(self, Nphase):
+        """generate the profile
+
+        This is implemented by the subclasses!
+
+        Args:
+            Nphase (int): number of phase bins
+        """
+        raise NotImplementedError()
+
+
+    def _make_amp_pulse(self, samprate, period, Npulse=1):
+        """generate an amplitude pulse
+
+        This method should be used for radio frequency and basebanded
+        pulses.
+
+        Args:
+            samprate (float): sample rate (MHz)
+            period (float): pulse period (sec)
+            Npulse (int): number of pulses to generate
+        """
+        sr = make_quant(samprate, "MHz")
+        P = make_quant(period, "s")
+        Nsamp = int((sr * P).decompose())
+        if not hasattr(self, '_profile'):
+            # profile not yet generated
+            self.gen_profile(Nsamp)
+        elif len(self._profile) != Nsamp:
+            # profile at wrong sample rate
+            self.gen_profile(Nsamp)
+        self.gen_profile(Nsamp)
+
+        Nchunk = 1 # int(__MAX_AR_SIZE__ // self.NRows)
+
+        pr = np.tile(self.profile, reps)
+        L = pr.shape
+        pulse = pr * signal._draw_norm * np.random.normal(0, self.gauss_draw_sigma, L) #pull from gaussian distribution
+
+
+        raise NotImplementedError()
+
+    def _make_pow_pulse(self):
+        """generate a power pulse
         
-        Required Args:
-            signal (:class:`Signal`-like): signal object to store pulses
-            tobs (float): observation time (sec)
-            start (float): start time
-        """
-
-        if signal.sigtype is "RFSignal":
-            self._make_rf_pulses()
-        elif signal.sigtype is "BasebandSignal":
-            self._make_bb_pulses()
-        elif signal.sigtype is "FilterBankSignal":
-            self._make_fb_pulses()
-        else:
-            raise NotImplementedError()
-
-    def _make_rf_pulses(self):
-        """generate radio frequency pulses"""
+        This method should be used for filter bank pulses"""
         raise NotImplementedError()
-
-    def _make_bb_pulses(self):
-        """generate baseband pulses"""
-        raise NotImplementedError()
-
-    def _make_fb_pulses(self):
-        """generate filter bank pulses"""
-        raise NotImplementedError()
-
-
-    def make_pulses_old(self, start_time = 0, stop_time = None):
-        """Function that makes pulses using the defined profile template.
-        Note: 'intensity'-type signals pulled from a gamma distribution using draw_intensity_pulse(),
-            'voltage'-type signals pulled from a gaussian distribution using draw_voltage_pulse().
-        """
-
-        if stop_time == None:
-            stop_time = self.ObsTime
-        start_bin = int(start_time // self.TimeBinSize )
-        last_bin = int(stop_time // self.TimeBinSize )
-        if stop_time == self.ObsTime and start_time == 0:
-            N_periods_to_make = self.NPeriods
-            delta_bins = self.Nt
-        elif stop_time < self.ObsTime:
-            delta_bins = last_bin - start_bin
-            N_periods_to_make = int(delta_bins // self.nBinsPeriod)
-        elif stop_time > self.ObsTime:
-            last_bin = self.Nt
-            delta_bins = last_bin - start_bin
-            N_periods_to_make = int(delta_bins // self.nBinsPeriod)
-
-        self.NLastPeriodBins = int(delta_bins % self.nBinsPeriod)#delta_bins - N_periods_to_make * self.nBinsPeriod #Length of last period
-        pulseType = {"intensity":"draw_intensity_pulse", "voltage":"draw_voltage_pulse"}
-        pulseTypeMethod = getattr(self, pulseType[self.SignalType])
-
-        Nt_chunk = int(self.mem_size_limit // self.NRows)
-        self.ChunkSize = int(Nt_chunk // self.nBinsPeriod) # Calc ChunkSize in integer number of periods
-
-
-        if self.Nt * self.NRows > self.mem_size_limit and self.ChunkSize != 0 : #Limits the array size to 2.048 GB
-            """The following limits the length of the arrays that we call from pulseTypeMethod(), by limiting the number
-            of periods we pull from the distribution at one time. This is for machines with small amounts of memory, and
-            is currently optimized for an 8GB RAM machine. In this process, most of the time is spent writing to disk.
-            """
-
-            self.Nchunks = int(N_periods_to_make // self.ChunkSize)
-            self.NPeriodRemainder = int(N_periods_to_make % self.ChunkSize)
-            pulse_start = time.time()
-
-            for ii in range(self.Nchunks): #limits size of the array in memory
-                self.signal[:, start_bin + ii * self.ChunkSize * self.nBinsPeriod : \
-                            start_bin + (ii+1) * self.ChunkSize * self.nBinsPeriod] \
-                            = pulseTypeMethod(self.ChunkSize)
-                pulse_check = time.time()
-                try: #Python 2 workaround. Python 2 __future__ does not have 'flush' kwarg.
-                    print('\r{0:2.0}% sampled in {1:.2f} seconds.'.format((ii + 1)*100/self.Nchunks , pulse_check-pulse_start), end='', flush=True)
-                except: #This is the Python 2 version.
-                    print('\r{0:2.0}% sampled in {1:.2f} seconds.'.format((ii + 1)*100/self.Nchunks , pulse_check-pulse_start), end='')
-                    sys.stdout.flush()
-
-            if self.NPeriodRemainder != 0 :
-                self.signal[:,start_bin + self.Nchunks * self.ChunkSize * self.nBinsPeriod : start_bin + (self.Nchunks * self.ChunkSize + self.NPeriodRemainder) * self.nBinsPeriod] = pulseTypeMethod(self.NPeriodRemainder)
-
-        else:
-            self.signal[:,start_bin:N_periods_to_make * self.nBinsPeriod] = pulseTypeMethod(N_periods_to_make) #Can be put into main flow for large RAM computers.
-
-        self.LastPeriod = pulseTypeMethod(1)[:,0:self.NLastPeriodBins]
-        self.signal[:,start_bin + N_periods_to_make * self.nBinsPeriod:start_bin + N_periods_to_make * self.nBinsPeriod + self.NLastPeriodBins] = self.LastPeriod
 
 
 class GaussProfile(PulseProfile):
@@ -109,33 +83,57 @@ class GaussProfile(PulseProfile):
 
     Required Args:
         N/A
-    
+
     Optional Args:
         peak (float): center of gaussian in pulse phase, default: ``0.5``
         width (float): stdev of pulse in pulse phase, default: ``0.1``
         amp (float): amplitude of pulse relative to other pulses,
             default: ``1``
-        Nphase (int): number of phase bins, default: ``256``
-    
+
     Pulses are normalized so that maximum is 1.
     See draw_voltage_pulse, draw_intensity_pulse and make_pulses() methods for more details.
     """
-    def __init__(self, peak=0.5, width=0.1, amp=1, Nphase=256):
+    def __init__(self, peak=0.5, width=0.05, amp=1):
         #TODO: error checking for array length consistency?
         #TODO: if any param is a not array, then broadcast to all entries of other arrays?
 
-        peak = np.array(peak)
-        width = np.array(width)
-        amp = np.array(amp)
+        self._peak = peak
+        self._width =width
+        self._amp = amp
 
-        Amax = amp.max()
-
+    def gen_profile(self, Nphase):
+        """generate the profile
+        Args:
+            Nphase (int): number of phase bins
+        """
         profile = np.zeros(Nphase)
         ph = np.arange(Nphase)/Nphase
-        for p, sig, A in zip(peak, width, amp):
-            profile += A/Amax * np.exp(-0.5 * ((ph-p)/sig)**2)
+        Amax = np.max(self.amp)
+        try:
+            for p, wid, A in zip(self.peak, self.width, self.amp):
+                profile += A/Amax * np.exp(-0.5 * ((ph-p)/wid)**2)
+        except TypeError:
+            profile += (self.amp/Amax * 
+                        np.exp(-0.5 * ((ph-self.peak)/self.width)**2))
 
-        raise NotImplementedError()
+        self._profile = profile
+
+    @property 
+    def profile(self):
+        return self._profile
+
+    @property
+    def peak(self):
+        return self._peak
+
+    @property
+    def width(self):
+        return self._width
+
+    @property
+    def amp(self):
+        return self._amp
+
 
 class UserProfile(PulseProfile):
     """user specified pulse profile"""
--- psrsigsim/pulsar/pulsar.py
+++ psrsigsim/pulsar/pulsar.py
@@ -2,18 +2,125 @@
 from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 import numpy as np
+from scipy import stats
+from .profiles import GaussProfile
+from ..utils.utils import make_quant
 
 class Pulsar(object):
     """class for pulsars
-    
-    The minimal data to instatiate a pulsar is the period, intensity and
-    pulse profile. The profile is supplied via a :class:`PulseProfile`-like
+
+    The minimal data to instatiate a pulsar is the period, intensity, and
+    pulse profile. The Profile is supplied via a :class:`PulseProfile`-like
     object.
 
-    Other data could be supplied via a `.par` file.
-
+    Args:
+        period (float): pulse period (sec)
+        intensity (float): mean pulse intensity (Jy)
+        profile (:class:`PulseProfile`): pulse profile or 2-D pulse portrait
+        name (string): name of pulsar
     """
-    def __init__(self, profile=None):
+    #TODO Other data could be supplied via a `.par` file.
+    def __init__(self, period, intensity, profile=None, name=None):
+        self._period = make_quant(period, 's')
+        self._intensity = make_quant(intensity, 'Jy')
+
+        self._name = name
+
         if profile is None:
-            self._profile = GaussProfile()
-        raise NotImplementedError()
+            self._Profile = GaussProfile()
+        else:
+            self._Profile = profile
+
+    def __repr__(self):
+        namestr = "" if self._name is None else name+", "
+        return "Pulsar("+namestr+"{})".format(self.period.to('ms'))
+
+    @property
+    def Profile(self):
+        return self._Profile
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def period(self):
+        return self._period
+
+    @property
+    def intensity(self):
+        return self._intensity
+
+    def make_pulses(self, signal, tobs):
+        """generate pulses from Profile, :class:`PulseProfile` object
+
+        Required Args:
+            signal (:class:`Signal`-like): signal object to store pulses
+            tobs (float): observation time (sec)
+        """
+        tobs = make_quant(tobs, 's')
+
+        # select pulse generation method
+        if signal.sigtype in ["RFSignal", "BasebandSignal"]:
+            self._make_amp_pulses(signal, tobs)
+        elif signal.sigtype is "FilterBankSignal":
+            self._make_pow_pulses(signal, tobs)
+        else:
+            msg = "no pulse method for signal: {}".format(signal.sigtype)
+            raise NotImplementedError(msg)
+
+    def _make_amp_pulses(self, signal, tobs):
+        """generate amplitude pulses
+
+        This method should be used for radio frequency and basebanded
+        pulses.
+
+        Args:
+            signal (:class:`Signal`-like): signal object to store pulses
+            tobs (float): observation time (sec)
+        """
+        # loop over tobs generating single pulses
+        # TODO: if using a pulsar ephemeris adjust the single pulse spacing!
+
+        # N samples in observation
+        Nsamp_obs = int((tobs * signal.samprate).decompose())
+        signal.init_data(Nsamp_obs)
+        # N samples per pulse period
+        Nsamp_period = int((self.period * signal.samprate).decompose())
+        # N samples per memory chunk
+        Nsamp_chunk = int(__MAX_AR_SIZE__ // self.Nchan)
+
+        # break calculation into N chunks
+        Nchunk = Nsamp_obs // Nsamp_chunk +1
+
+        # number of pulse periods per memory chunk
+        Np_chunk = Nsamp_chunk // Nsamp_period
+
+
+    def _make_pow_pulses(self, signal, tobs):
+        """generate a power pulse
+
+        This method should be used for filter bank pulses
+
+        Args:
+            signal (:class:`Signal`-like): signal object to store pulses
+            tobs (float): observation time (sec)
+        """
+        #TODO this works for float32 ONLY, fix for int8
+        # i.e. use _draw_norm...
+        Nperiod = (tobs / self.period).decompose()
+        Nph = int((signal.samprate * self.period).decompose())
+        sngl_prof = self.Profile(Nph)
+        if signal.subint:
+            # generate one pulse in phase
+            distr = stats.chi2(df=Nperiod)
+            signal._set_draw_norm(df=Nperiod)
+
+            signal.init_data(Nph)
+            signal._data = sngl_prof * distr.rvs(size=signal.data.shape)
+        else:
+            # generate several pulses in time
+            distr = stats.chi2(df=1)
+            Nsamp = int((tobs / signal.samprate).decompose())
+            signal.init_data(Nsamp)
+
