4659b22923a7568712bdf00e1c0feab28f306056
Paul T. Baker <paultbaker@gmail.com>
2019-10-07 12:48:50 -0400

    working DataProfile

--- psrsigsim/pulsar/__init__.py
+++ psrsigsim/pulsar/__init__.py
@@ -1,12 +1,13 @@
 
 from .pulsar import Pulsar
-from .profiles import GaussProfile, UserProfile, PulseProfile
+from .profiles import PulseProfile, GaussProfile, UserProfile, DataProfile
 from .portraits import GaussPortrait, UserPortrait
 
 __all__ = ["Pulsar",
            "PulseProfile",
            "GaussProfile",
            "UserProfile",
+           "DataProfile",
            "GaussPortrait",
            "UserPortrait",
            ]
