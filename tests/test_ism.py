928db671901a19bb068b175858dc1243476bb80f
Hazboun6 <jeffrey.hazboun@gmail.com>
2019-10-07 10:09:48 -0700

    Made simple tests that work.

---
+++ tests/test_ism.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `psrsigsim.ism` module."""
+
+import pytest
+import psrsigsim as pss
+import psrsigsim.signal as sig
+import psrsigsim.pulsar as psr
+import psrsigsim.ism as ism
+
+from psrsigsim.signal.fb_signal import FilterBankSignal
+from psrsigsim.pulsar.pulsar import Pulsar
+from psrsigsim.ism.ism import ISM
+from psrsigsim.utils.utils import make_quant
+
+@pytest.fixture
+def signal():
+    """
+    Fixture signal class
+    """
+    fbsig = FilterBankSignal(1400,400)
+    return fbsig
+
+@pytest.fixture
+def pulsar():
+    """
+    Fixture pulsar class
+    """
+    period = make_quant(5,'ms')
+    return Pulsar(period,10,name='J1746-0118')
+
+@pytest.fixture
+def ism():
+    """
+    Fixture ism class
+    """
+    return ISM()
+
+def test_disperse(signal,pulsar,ism):
+    """"""
+    tobs = make_quant(5,'s')
+    pulsar.make_pulses(signal,tobs)
+    ism.disperse(signal,10)
