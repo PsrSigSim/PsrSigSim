c27eed3e6c50c65c77e9ae2fc4c5a1558567ffd5
Brent Shapiro-Albert <bshapiroalbert@ip-25-219.wvu.edu>
2020-01-27 11:09:37 -0500

    Added txtfile class to save simulated signals as text files for certain convenience classes. Also added tests.

--- tests/test_io.py
+++ tests/test_io.py
@@ -13,6 +13,17 @@ from psrsigsim.signal.fb_signal import FilterBankSignal
 from psrsigsim.pulsar.pulsar import Pulsar
 from psrsigsim.io.psrfits import PSRFITS
 from psrsigsim.utils.utils import make_quant
+from psrsigsim.io.txtfile import TxtFile
+
+@pytest.fixture
+def signal():
+    """
+    Fixture signal class
+    """
+    fbsig = FilterBankSignal(1400,400,Nsubband=2,\
+                             sample_rate=186.49408124993144*2048*10**-6,\
+                             sublen=0.5)
+    return fbsig
 
 @pytest.fixture
 def pulsar():
@@ -31,6 +42,14 @@ def PSRfits():
     fitspath = "data/test.fits"
     tempfits = "data/B1855+09.L-wide.PUPPI.11y.x.sum.sm"
     return PSRFITS(path=fitspath, template=tempfits, fits_mode='copy')
+
+@pytest.fixture
+def TXTfile():
+    """
+    Fixture txtfile class
+    """
+    pdvpath = "data/test_pdv"
+    return TxtFile(path=pdvpath)
 
 def test_fitssig(PSRfits):
     """
@@ -76,3 +95,10 @@ def test_savephaseconnect_inc(PSRfits, pulsar):
                  MJD_start = 55999.9861+30.0, inc_len = 30.0, ref_MJD = 56000.0, \
                  usePint = True)
     os.remove("data/test.fits")
+    
+def test_savepdv(TXTfile, signal, pulsar):
+    tobs = make_quant(1,'s')
+    pulsar.make_pulses(signal,tobs)
+    TXTfile.save_psrchive_pdv(signal, pulsar)
+    os.remove("data/test_pdv_0.txt")
+
