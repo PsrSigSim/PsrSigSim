976fc4cfde781bc0ca4396b3c43b22d92f5222ac
Hazboun6 <jeffrey.hazboun@gmail.com>
2020-02-04 16:51:46 -0800

    Added a few more tests.

--- tests/test_portraits.py
+++ tests/test_portraits.py
@@ -44,12 +44,33 @@ def pulsar():
 def test_profile1(j1713_profile):
     pr = DataProfile(j1713_profile,phases=None)
     pr.init_profiles(100)
+    assert(hasattr(pr,'_profiles'))
+    assert(hasattr(pr,'_max_profile'))
 
 def test_profile2(j1713_profile):
     pr = DataProfile(j1713_profile,phases=None)
     pr.init_profiles(100, Nchan=20)
+    assert(hasattr(pr,'_profiles'))
+    assert(hasattr(pr,'_max_profile'))
 
 def test_profile3(j1713_profile):
     ph = np.linspace(0,1,2048)
     pr = DataProfile(j1713_profile,phases=ph)
     pr.init_profiles(100, Nchan=20)
+    assert(hasattr(pr,'_profiles'))
+    assert(hasattr(pr,'_max_profile'))
+
+def test_portrait1(j1713_profile):
+    dataport = np.tile(j1713_profile,(10,1))
+    ph = np.linspace(0,1,2048)
+    port = DataPortrait(dataport, phases=ph)
+    port.init_profiles(100)
+    assert(hasattr(port,'_profiles'))
+    assert(hasattr(port,'_max_profile'))
+
+def test_portrait2(j1713_profile):
+    dataport = np.tile(j1713_profile,(10,1))
+    port = DataPortrait(dataport)
+    port.init_profiles(100)
+    assert(hasattr(port,'_profiles'))
+    assert(hasattr(port,'_max_profile'))
