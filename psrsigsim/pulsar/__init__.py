dd7b30408e8aea044cd9b8b8cea5b2d5d186f525
Hazboun6 <jeffrey.hazboun@gmail.com>
2020-07-06 17:41:03 -0700

    change order in __all__ for docstrings

--- psrsigsim/pulsar/__init__.py
+++ psrsigsim/pulsar/__init__.py
@@ -4,11 +4,12 @@ from .profiles import PulseProfile, GaussProfile, UserProfile, DataProfile
 from .portraits import GaussPortrait, UserPortrait, DataPortrait
 
 __all__ = ["Pulsar",
+           "PulsePortrait",
+           "GaussPortrait",
+           "UserPortrait",
+           "DataPortrait",
            "PulseProfile",
            "GaussProfile",
            "UserProfile",
            "DataProfile",
-           "GaussPortrait",
-           "UserPortrait",
-           "DataPortrait",
            ]
